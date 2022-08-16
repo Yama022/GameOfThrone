b62d513747e50d4931bc3b70a2c5fb934623bd35
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-08-16 09:53:08 +0200

    update detail.js + style

--- src/components/Characters/detail.js
+++ src/components/Characters/detail.js
@@ -13,6 +13,7 @@ export default function Detail() {
       .then((response) => {
         const { data } = response;
         setCharacters(data);
+        console.log(data, 'data');
       }).catch((error) => {
         console.log(error);
       });
@@ -31,41 +32,107 @@ export default function Detail() {
           <h2>{characters?.name}</h2>
         </div>
 
+        <div className="detail__content__title">
+          <h3>
+            {
+              characters?.titles.every((title) => title != 0) ? 
+              <span>Title : </span> : <span></span>}
+              {characters?.titles}
+            
+          </h3>
+        </div>
+
+        <div className="detail__content__culture">
+          <h3>
+            {
+              characters?.culture != 0 ?
+              <span>Culture : </span> : <span></span>}
+              {characters?.culture}
+          </h3>
+        </div>
+
         <div className="detail__content__aliases">
           {
-            
             characters?.aliases.every(x => x.length>0) && (
             <h3>
-              {characters?.aliases.length > 1 ?
-              "Aliases " : "Alias "}
-              : {characters?.aliases }
+              <span>{characters?.aliases.length > 1 ?
+              "Aliases " : "Alias "}</span>
+              : {characters?.aliases.join(', ') }
             </h3> 
             )
           }
         </div>
 
+        <div className="detail__content__allegiances">
+          {
+            characters?.allegiances.every(x => x.length>0) && (
+            <h3>
+              <span>
+                {characters?.allegiances.length > 1 ?
+                  "Allegiances " : "Allegiance "}
+              </span>
+              : {characters?.allegiances.join(', ').replaceAll('https://anapioficeandfire.com/api/', '').replaceAll('s/', ' ') }
+            </h3>
+            )
+          }
+        </div>
+
         <div className="detail__content__actor">
-          <h3>{characters?.playedBy}</h3>
+          <h3>
+          {
+              characters?.playedBy != 0 ?
+              <span>Played by : </span> : <span></span>}
+              {characters?.playedBy}
+            </h3>
         </div>
 
         <div className="detail__content__gender">
-          <h3>{characters?.gender}</h3>
+          <h3>
+            {characters?.gender != 0 ?
+              <span> Gender : </span> : <span></span>}
+              {characters?.gender}
+          </h3>
         </div>
 
         <div className="detail__content__born">
-          <h3>{characters?.born}</h3>
+          <h3>
+            {characters?.born.length != 0 ?
+              <span>Born to : </span> : <span></span>}
+            {characters?.born}
+          </h3>
         </div>
 
         <div className="detail__content__died">
-          <h3>{characters?.died}</h3>
+          <h3>
+            {characters?.died.length != 0 ?
+              <span>Died in : </span> : <span></span>}
+            {characters?.died}
+          </h3>
         </div>
 
         <div className="detail__content__books">
-          <h3>{characters?.books.join(', ').replaceAll('https://anapioficeandfire.com/api/', '').replaceAll('s/', ' ')}</h3>
+            {characters?.books.every(x => x.length>0) && (
+          <h3>
+              <span>
+                {characters?.books.length > 1 ?
+                  "Books " : "Book "}
+              </span>
+            : {characters?.books.join(', ').replaceAll('https://anapioficeandfire.com/api/', '').replaceAll('s/', ' ')}
+          </h3>
+            )}
         </div>
 
         <div className="detail__content__tv">
-          <h3>{characters?.tvSeries}</h3>
+          {characters?.tvSeries.every(x => x.length>0) && (
+          <h3>
+              <span>
+                {characters?.tvSeries.length > 1 ?
+                  "TV Series " : "TV Series "}
+              </span>
+            : {characters?.tvSeries.join(', ').replaceAll('https://anapioficeandfire.com/api/', '').replaceAll('s/', ' ')}
+          
+          </h3>
+          )}
         </div>
 
       </div>
